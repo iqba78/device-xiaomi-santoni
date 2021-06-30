8b0cae577d05dfaa21d0c89b401f667460ee1d50
ikeh045 <njkampret@gmail.com>
2021-06-30 05:37:29 +0300

    nusantara unof ify

--- device.mk
+++ device.mk
@@ -29,9 +29,6 @@ PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8996/media
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_PACKAGES += \
-    NusantaraPapers
-
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
